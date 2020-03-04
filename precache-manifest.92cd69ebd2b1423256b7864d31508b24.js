0715ecbae997de5a72cc37d9e22c21cddd33f850
kp98pandey <kp98pandey@gmail.com>
2020-03-04 17:43:41 +0530

    Updates

---
+++ precache-manifest.92cd69ebd2b1423256b7864d31508b24.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b30c61e62f87fa51c5085b8ac3d23a6",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "ea5a6b2d3b5c84666853",
+    "url": "/main/static/css/2.6a669704.chunk.css"
+  },
+  {
+    "revision": "b399f8af8bc82c896d1c",
+    "url": "/main/static/css/main.0a105ac9.chunk.css"
+  },
+  {
+    "revision": "ea5a6b2d3b5c84666853",
+    "url": "/main/static/js/2.62473855.chunk.js"
+  },
+  {
+    "revision": "cb5cd76ba356c384223d7ba22549acd6",
+    "url": "/main/static/js/2.62473855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b399f8af8bc82c896d1c",
+    "url": "/main/static/js/main.f502043c.chunk.js"
+  },
+  {
+    "revision": "99c5ba76eb45fc3182eb",
+    "url": "/main/static/js/runtime-main.4e1ce923.js"
+  },
+  {
+    "revision": "24e6392a21f900a3bf6b7988ecba54e9",
+    "url": "/main/static/media/Admin.24e6392a.jpg"
+  },
+  {
+    "revision": "1cc6dfe7af3e170f2b6372719515645d",
+    "url": "/main/static/media/SignIn.1cc6dfe7.jpeg"
+  },
+  {
+    "revision": "1286a19fc578e6b5a92e6dcb744dec16",
+    "url": "/main/static/media/dxicons.1286a19f.woff2"
+  },
+  {
+    "revision": "147eb6fe61ff3af12cd8763022ae7f24",
+    "url": "/main/static/media/dxicons.147eb6fe.ttf"
+  },
+  {
+    "revision": "2b145cefd95f699e2eecc965fb38c8a8",
+    "url": "/main/static/media/dxicons.2b145cef.woff"
+  },
+  {
+    "revision": "ac68a41758e408a12ff4a7aec35e44b0",
+    "url": "/main/static/media/menu.ac68a417.jpeg"
+  }
+]);
